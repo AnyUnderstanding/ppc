de7a171017388962920ae244a905be3f2c4c0906
Christopher <christopher.alan.w@gmail.com>
2022-08-25 14:30:02 +0200

    refactored color slider and made the selector move by click

--- src/main/kotlin/ui/documentView/toolbar/colorpicker/ColorSlider.kt
+++ src/main/kotlin/ui/documentView/toolbar/colorpicker/ColorSlider.kt
@@ -1,29 +1,26 @@
 package ui.documentView.toolbar.colorpicker
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Divider
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.consumeAllChanges
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.*
-import androidx.compose.ui.layout.boundsInRoot
+import androidx.compose.ui.input.pointer.PointerEventType
+import androidx.compose.ui.input.pointer.onPointerEvent
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.IntOffset
@@ -33,11 +30,12 @@ import util.ColorVector
 import kotlin.math.floor
 import kotlin.math.roundToInt
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun ColorSlider(DocumentController: DocumentController) {
 
     var value by remember { mutableStateOf(0f) }
-    var diameter = 15
+    var size = 15
 
     val rainbowColors = listOf(
         Color(0xFFFF0000),
@@ -58,38 +56,45 @@ fun ColorSlider(DocumentController: DocumentController) {
         modifier = Modifier.height(200.dp).width(300.dp),
         contentAlignment = Alignment.Center
     ) {
-        var offsetX by remember { mutableStateOf(0f) }
-        var width by remember { mutableStateOf(0) }
-
+        var selectorOffsetX by remember { mutableStateOf(0f) }
+        var width by remember { mutableStateOf(1) }
         Box(
-            modifier = Modifier.clip(RoundedCornerShape(15.dp)).fillMaxWidth(0.5f).background(rainbowBrush)
-                .height(diameter.dp).onGloballyPositioned {
+            modifier = Modifier.clip(RoundedCornerShape(size.dp)).fillMaxWidth(0.5f).background(rainbowBrush)
+                .height(size.dp).onGloballyPositioned {
                     width = (it.size.width - it.size.height) // height is equal the diameter of the circle
-                }) {
+                }
+                .onPointerEvent(PointerEventType.Press){
+                    selectorOffsetX = it.changes.first().position.x - size/2
+                }
+        ) {
             Box(
                 Modifier
-                    .offset { IntOffset(offsetX.roundToInt(), 0) }
-                    .size(diameter.dp)
+                    .offset { IntOffset(selectorOffsetX.roundToInt(), 0) }
+                    .size(size.dp)
                     .pointerInput(Unit) {
                         detectDragGestures { change, dragAmount ->
-                            change.consumeAllChanges()
-                            offsetX += dragAmount.x
-                            if (offsetX > width) offsetX = width.toFloat()
-                            else if (offsetX < 0f) offsetX = 0f
+                            change.consume()
+                            selectorOffsetX += dragAmount.x
 
-                            value = (0.9f * offsetX) / width
                         }
                     }
             ) {
-                Donut()
+                Donut(getColorAt(value, rainbowColors), size.toFloat(), size.toFloat() / 8)
             }
         }
+        if (selectorOffsetX > width) selectorOffsetX = width.toFloat()
+        else if (selectorOffsetX < 0f) selectorOffsetX = 0f
+        value = (0.9f * selectorOffsetX) / width
+
     }
 
     DocumentController.setColor(getColorAt(value, rainbowColors))
 
 
 }
+
+
+
 
 fun getColorAt(value: Float, colors: List<Color>): Color {
     val i = (floor((value / 0.1))).toInt()
@@ -103,28 +108,29 @@ fun getColorAt(value: Float, colors: List<Color>): Color {
 }
 
 @Composable
-fun Donut() {
-    Surface(modifier = Modifier.size(20.dp),
+fun Donut(color: Color, r: Float, thickness: Float) {
+
+
+    val p1 = circlePath(r)
+    val p2 = circlePath(r - thickness,thickness, thickness)
+    Surface(modifier = Modifier.size(r.dp),
         color = Color.White,
         shape = object : Shape {
             override fun createOutline(size: Size, layoutDirection: LayoutDirection, density: Density): Outline {
-                val thickness = size.height / 8
-                val p1 = Path().apply {
-                    addOval(Rect(0f, 0f, size.width - 1, size.height - 1))
-                }
-                val p2 = Path().apply {
-                    addOval(
-                        Rect(
-                            thickness,
-                            thickness,
-                            size.width - 1 - thickness,
-                            size.height - 1 - thickness
-                        )
-                    )
-                }
                 val p3 = Path()
-                p3.op(p1, p2, PathOperation.difference)
+                p3.op(p1, p2, PathOperation.Difference)
                 return Outline.Generic(p3)
+            }
+
+        }
+    ) {
+    }
+
+    Surface(modifier = Modifier.size(r.dp),
+        color = color,
+        shape = object : Shape {
+            override fun createOutline(size: Size, layoutDirection: LayoutDirection, density: Density): Outline {
+                return Outline.Generic(p2)
             }
 
         }
@@ -132,4 +138,12 @@ fun Donut() {
     }
 }
 
+
+
+
+fun circlePath(r: Float, x: Float = 0f, y: Float = 0f): Path {
+    return Path().apply {
+        addOval(Rect(x, y, r, r))
+    }
+}
 fun Color.toColorVector() = ColorVector(alpha, red, green, blue)
